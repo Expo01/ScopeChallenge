58cf937d6ade8341818594ea310c05490cf66e11
Expo <crdailey01@gmail.com>
2023-02-19 09:13:07 -0800

    contrived us of same variable name for Class, class instance, class field, Scanner instance, for loop local variable

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        com.timbuchalka.X x = new com.timbuchalka.X(new Scanner(System.in));
+        x.x();
+    }
+}
